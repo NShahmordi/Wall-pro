793ee535bdc7159fdb3e9a684b4492faa15ac8b0
Narges Shahmoradi <Narcissus.nsh@gmail.com>
2024-12-14 21:12:27 +0330

    Add:attributes to CustomUserAdmin & CustomAdvertisementAdmin classes in admin file

--- Wall/Main/admin.py
+++ Wall/Main/admin.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
+from .models import Users,Advertisement 
 
 # Register your models here.
-from .models import Users,Advertisement
-
 @admin.register(Users)
 class CustomUserAdmin(admin.ModelAdmin):
     list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
-    
-    
-
+    prepopulated_fields = {'slug': ('username',)}
 @admin.register(Advertisement)
 class CustomAdvertisementAdmin(admin.ModelAdmin):
     list_display = ('title', 'owner','category' , 'city')
+    prepopulated_fields = {'slug': ('title', 'category', 'city')}
+    search_fields = ('title', 'city', 'category')
+    list_filter = ('city', 'category')
+
