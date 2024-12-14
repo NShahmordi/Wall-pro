85f47e6aa085d34fb2993b1fae0a4e9dca21ee82
Narges Shahmoradi <Narcissus.nsh@gmail.com>
2024-12-14 14:10:05 +0330

    fix: Advertisment and Users  models

--- Wall/Main/admin.py
+++ Wall/Main/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Users,Advertisement
+
+@admin.register(Users)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
+    
+    
+
+@admin.register(Advertisement)
+class CustomAdvertisementAdmin(admin.ModelAdmin):
+    list_display = ('title', 'owner','category' , 'city')
--- Wall/Main/models.py
+++ Wall/Main/models.py
@@ -1,15 +1,13 @@
 from django.db import models
-
-# our model
-from django.db import models
 from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 
 # Our models
-class Users(models.Model):
+class Users(AbstractUser):
     id = models.AutoField(primary_key=True)
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=30, blank=True)
-    phone_number = models.CharField(max_length=20, primary_key=True)
+    phone_number = models.CharField(max_length=20)
     email = models.EmailField(unique=True, blank=True)
     slug = models.SlugField(max_length=200, unique=True)
     
@@ -17,37 +15,35 @@ class Users(models.Model):
         return self.email
 
 class Category(models.Model):
-    category_name = models.ForeignKey(max_length=20, null=True, blank=True)
+    name = models.CharField(max_length=20, null=True, blank=True)
     def __str__(self):
-        return self.category 
+        return self.name
 
 class City(models.Model):
-    city_name = models.ForeignKey(max_length=20, null=True,blank=True)
+    name = models.CharField(max_length=20, null=True, blank=True)
     def __str__(self):
-        return self.city
-    
+        return self.name
+
 class Advertisement(models.Model):
     title = models.CharField(max_length=50, null=True, blank=True)
-    description = models.TextField(max_length=400,null=True, blank=True)
+    description = models.TextField(max_length=400, null=True, blank=True)
     price = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     publication_date = models.DateField()
     slug = models.SlugField(max_length=200, unique=True)
     owner = models.ForeignKey(
         settings.AUTH_USER_MODEL,
-        ralated_name='advertisement',
+        related_name='advertisement',
         on_delete=models.CASCADE,
     )
     city = models.ForeignKey(City, on_delete=models.CASCADE, null=True, blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
     SHIRT_SIZES = (
-        #The existence of each ad has three states:
         ('N', 'New'),
         ('as_new', 'as new'), 
         ('U', 'Used'),
-    ) 
-    #For each ad, the user can select a status->(max_length=1):
-    shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZES)  
+    )
+    shirt_size = models.CharField(max_length=6, choices=SHIRT_SIZES)  
         
     class Meta:
         ordering = ['-created_at']   
@@ -56,4 +52,4 @@ class Advertisement(models.Model):
         ]
     
     def __str__(self):
-        return self.title    
+        return self.title
