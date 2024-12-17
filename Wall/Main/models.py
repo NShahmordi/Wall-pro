f0bcd95854a6a00512824a5ec2e966e1f681213e
Narges Shahmoradi <Narcissus.nsh@gmail.com>
2024-12-18 00:03:10 +0330

    Fix: City & Category models

--- Wall/Main/models.py
+++ Wall/Main/models.py
@@ -30,12 +30,12 @@ class Users(AbstractBaseUser, PermissionsMixin):
 class Category(models.Model):
     category_name = models.CharField(max_length=20, null=True, blank=True)
     def __str__(self):
-        return self.name
+        return self.category_name
 
 class City(models.Model):
     city_name = models.CharField(max_length=20, null=True, blank=True)
     def __str__(self):
-        return self.name
+        return self.city_name
 
 class Advertisement(models.Model):
     title = models.CharField(max_length=50, null=True, blank=True)
